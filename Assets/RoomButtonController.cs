3b62d1ac4dfcec8252dcd0691dd6445c1d760625
slrlx <108903798+slrlx@users.noreply.github.com>
2025-05-11 14:36:17 +0900

    이동화면 안정화,이동버튼 클릭시 이동버튼 나타남, 다른 배경 누를시 이동버튼 취소

---
+++ Assets/RoomButtonController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:061279382061ef8235ef036b15338a1a4bcb32fb681c8627fdb4cd289639ca05
+size 798
--- Assets/RoomTransition.cs
+++ Assets/RoomTransition.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58acfe1d64e9dc2bc9766c4cab40860ce966ed7b1fd110941f0b0fcd50a29c61
-size 2343
+oid sha256:b47ea9dd96e92f7b317064ea9dc21a341c912b5fc7f0d6be1b7324a981933859
+size 3040
